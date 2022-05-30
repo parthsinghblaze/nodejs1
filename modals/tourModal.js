78bbf64a5b9079c12c6eb70d6c487798e35e2573
parth <parthsingh1910@gmail.com>
2022-05-30 10:53:21 +0530

    modal done

--- modals/tourModal.js
+++ modals/tourModal.js
@@ -6,14 +6,52 @@ const tourSchema = mongoose.Schema({
         require: [true, "A tour must have a name"],
         unique: true
     },
-    rating: {
+    durations: {
         type: Number,
-        default: 5
+        require: [true, "A tour must have a duration"]
+    },
+    maxGroupSize: {
+        type: Number,
+        require: [true, "A tour must have a Max Group Size"]
+    },
+    difficulty: {
+        type: String,
+        require: [true, "A Tour must have a Difficulty"]
+    },
+    ratingAverage: {
+        type: Number,
+        default: 4.5
+    },
+    ratingQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         require: [true, "A Tour Must have a Price"]
-    }
+    },
+    priceDiscount: {
+        type: Number
+    },
+    summary: {
+        type: String,
+        trim: true,
+        require: [true, 'A tour must have summary']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        require: [true, "A tour must have a cover image"]
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
 })
 
 const Tour = mongoose.model("Tour", tourSchema)
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const app = require("./app")
+const dotenv = require("dotenv")
+
+dotenv.config({ path: "./config.env" })
 
 const port = process.env.PORT || 8001
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
